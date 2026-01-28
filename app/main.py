e7e7e667e9070088e072b37dc37b65c0ce848729
pradhanprac <pradhan.prac@northeastern.edu>
2026-01-28 14:00:51 -0500

    Update main app router configuration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from app.routers import dimension , assessments, companies
+from app.routers import dimension , assessments, companies, health
 
 app = FastAPI(
     title="PE Org-AI-R Platform Team-2",
     description="AI Rediness Platform"
 )
+app.include_router(health.router,prefix="/api/v1")
+
 app.include_router(dimension.router, prefix="/api/v1")
 
 app.include_router(assessments.router, prefix="/api/v1")
